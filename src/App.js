055e296065d8989d9394e83fabeceb95dfc12ce7
Renato Oliveira <renatog.oliveira17@gmail.com>
2023-05-25 15:50:50 -0300

    applying global style

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ const AppContainer = styled.div`
     width: 100vw;
     height: 100vh;
     background-image: linear-gradient(90deg, #002F52 35%, #326589);
-  li {
-    list-style: none;
-  }
+  
 `
 //eu posso fazer um export default para o AppContainer
 function App() {
